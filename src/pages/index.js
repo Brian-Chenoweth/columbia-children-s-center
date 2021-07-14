f8a0e141adb5033d64d1d143fda55b6f4caf425e
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2021-07-14 12:41:20 -0700

    Implement gatsby-plugin-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
        <p>Here is a child of the layout component.</p>
+       <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="../images/clifford-pitbull.jpeg"
+      />
     </Layout>
   )
 }
