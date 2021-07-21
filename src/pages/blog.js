2db6d3e646c766dcb1e8fa5bcf7f445c0f3f2f8a
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2021-07-21 16:12:03 -0700

    Render blog post content on blog index page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,19 +1,22 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ( {data} ) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}{node.ext}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>
+              {node.body}
+            </MDXRenderer>
+          </article>
         ))
       }
-      </ul>
     </Layout>
   )
 }
@@ -25,13 +28,18 @@ query {
       title
     }
   }
-  allFile(filter: {ext: {eq: ".mdx"}}) {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
     nodes {
-      name
-      ext
+      id
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      body
     }
   }
 }
+
  `
 
 export default BlogPage
