0064a72bbdc315a0c7225045bbac80ee1a9eb1df
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2021-07-21 13:43:08 -0700

    Use GraphQL to pull in site meta data stored in gatsby-config

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,33 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { Helmet } from 'react-helmet';
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
-  navLinkText
+  navLinkText,
+  siteTitle
 } from './layout.module.scss'
 
 const Layout = ({ pageTitle, children }) => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    title
+                }
+            }
+        }
+    `)
+
   return (
     <main className={container}>
-      <title>{pageTitle}</title>
+        <Helmet>
+            <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+        </Helmet>
+        <p className={siteTitle}>{data.site.siteMetadata.title}</p>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
