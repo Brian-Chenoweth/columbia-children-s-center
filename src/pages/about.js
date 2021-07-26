fe6ef033813224c976a54488433fe31968ffb1bc
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2021-07-26 12:17:40 -0700

    add const pageTitle

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,17 +5,18 @@ import AdayIn from '../components/aDayin'
 import TuitionRates from '../components/tuitionRates'
 
 const AboutPage = () => {
+  const pageTitle = "About Me";
   return (
-    <Layout pageTitle="About Me">
+    <Layout pageTitle={pageTitle}>
         <p>About page child of layout component.</p>
-        <AdayIn pageTitle="About Me" >
+        <AdayIn pageTitle={pageTitle} >
             <ul>
               <li><strong>7:00 - 8:00:</strong>lorem</li>
               <li><strong>7:00 - 8:00:</strong>lorem</li>
               <li><strong>7:00 - 8:00:</strong>lorem</li>
             </ul>
         </AdayIn>
-        <TuitionRates pageTitle="About Me" halfDay="$229.00" fullDay="$297.00"/>
+        <TuitionRates pageTitle={pageTitle} halfDay="$229.00" fullDay="$297.00"/>
         <Employee employeeName="Name test" employeeImg="img-test.jpg"/>
     </Layout>
   )
