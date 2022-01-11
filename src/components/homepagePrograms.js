97e1e1e50224a398cd485148bd4d9a6232fe3abf
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2022-01-11 15:38:08 -0800

    Work on front page

--- src/components/homepagePrograms.js
+++ src/components/homepagePrograms.js
@@ -2,11 +2,14 @@ import * as React from 'react'
 import {hero, heroProfileImg, imgWrap, heroDescriptionBk, heroDescription, heroDate, heroBtn } from './styles/homepagePrograms.module.scss'
 import { button } from '../components/styles/layout.module.scss'
 var youngToddler = require ('../images/home/young-toddler.jpg');
+var earlyYears = require ('../images/home/early-years.jpg');
 
 const homepagePrograms = ({ children }) => {
     
 
   return (
+    <div>
+
     <div className={hero}>
 
       <div className={imgWrap}>
@@ -20,7 +23,7 @@ const homepagePrograms = ({ children }) => {
         </div>
 
         <div className={heroDate}>
-          <p>18 months to 2½ years</p>
+          <p>18 months - 2½ years</p>
         </div>
 
         <a href="#" className={button}>Learn More</a>
@@ -28,6 +31,31 @@ const homepagePrograms = ({ children }) => {
       </div>
 
     </div>
+
+    <div className={hero}>
+
+    <div className={imgWrap}>
+      <img className={heroProfileImg} src={earlyYears.default} alt="" />
+    </div>
+
+    <div className={heroDescriptionBk}>
+
+      <div className={heroDescription}>
+        <h2>Early Years</h2>
+      </div>
+
+      <div className={heroDate}>
+        <p>2 years - 3 years</p>
+      </div>
+
+      <a href="#" className={button}>Learn More</a>
+
+    </div>
+
+</div>
+
+</div>
+
   )
 }
 
