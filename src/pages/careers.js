d122189281f09272353cfc2a7e4b2960969884c2
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2022-03-29 07:28:06 -0700

    Add content

--- src/pages/careers.js
+++ src/pages/careers.js
@@ -1,9 +1,24 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import JSONData from "../data/careers.json"
 
 const CareersPage = () => {
   return (
     <Layout pageTitle="Careers" metaDesc="Careers available at Columbia Children's Center.">
+      <p>Columbia Children's Center is a privately owned preschool established in 1992. We value the ideas and interests of the children in our care. We believe that children learn best through active participation with their environment and that the teacher's job is to provide a variety of opportunities and experiences to promote growth in all areas.</p>
+      <p>We are looking for honest, caring and dedicated employees to join our team! Applicants should be passionate about early childhood education and have a love for enriching the minds of little ones while guiding them through a philosophy that fosters their creativity and curiosities. Applicants should have a minimum of 12 units in Early Childhood Education or a willingness to complete accredited college or university courses in Early Childhood Education. We also look for applicants who have experience working with young children as this provides a solid foundation and understanding of the children in our care.</p>
+      {JSONData.careers.map((data, index) => {
+        return (
+          <div>
+            <p><strong><a href={`mailto:${data.email}`}>{data.email}</a></strong></p>
+            <p><strong>{data.title}</strong></p>
+            <p><strong>{data.group}</strong></p>
+            <p><strong>{data.description}</strong></p>
+            <p><strong>{data.time}</strong></p>
+            <p><strong>{data.email}</strong></p>
+          </div>
+        )
+      })}  
     </Layout>
   )
 }
--- src/pages/parents/covid.js
+++ src/pages/parents/covid.js
@@ -5,6 +5,25 @@ const CovidPage = () => {
   const pageTitle = "Covid";
   return (
     <Layout pageTitle={pageTitle} metaDesc="Covid protocols at Columbia Children's Center.">
+      <p>If your child tests positive or is exposed to someone who has tested positive, please keep them home and contact <a href="mailto:sean@columbiachildrenscenter.com">sean@columbiachildrenscenter.com</a> so he can determine when they can safely return to Columbia.</p>
+      <p>If your child has any of the following symptoms; please keep them home and contact Lisa via text <a href="sms:+18054480375">(805) 448-0375</a> or <a href="mailto:lisa@columbiachildrenscenter.com">lisa@columbiachildrenscenter.com</a> so we can determine when they can attend.</p>
+      <p>Symptoms include:</p>
+      <ul>
+        <li>fever</li>
+        <li>chills</li>
+        <li>cough</li>
+        <li>shortness of breath</li>
+        <li>fatigue</li>
+        <li>body aches</li>
+        <li>headache</li>
+        <li>loss of taste or smell</li>
+        <li>sore throat</li>
+        <li>congestion</li>
+        <li>runny nose</li>
+        <li>nausea</li>
+        <li>vomiting and diarrhea</li>
+      </ul>
+      <p>Please remember that we are having the children spend the majority of their day outside so please be sure to send them with jackets or sweaters!</p>
     </Layout>
   )
 }
