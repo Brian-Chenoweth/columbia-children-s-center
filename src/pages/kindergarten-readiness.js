50d463ea4a18584056dc396d5f0322c1beabdf88
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2023-03-12 13:40:16 -0700

    Slider images

--- src/pages/kindergarten-readiness.js
+++ src/pages/kindergarten-readiness.js
@@ -8,6 +8,20 @@ import JSONDat from ".././data/adayin/kindergarten-readiness.json"
 import { StaticImage } from 'gatsby-plugin-image'
 import { introImage } from '.././components/styles/page.module.scss'
 import { employeesWrap } from '.././components/styles/employee.module.scss'
+import Slider from "react-slick";
+import { Helmet } from 'react-helmet';
+import kindergartenReadiness1 from '.././images/programs/kindergarten-readiness/kindergarten-readiness1.jpg';
+import kindergartenReadiness2 from '.././images/programs/kindergarten-readiness/kindergarten-readiness2.jpg';
+import kindergartenReadiness3 from '.././images/programs/kindergarten-readiness/kindergarten-readiness3.jpg';
+import kindergartenReadiness4 from '.././images/programs/kindergarten-readiness/kindergarten-readiness4.jpg';
+import kindergartenReadiness5 from '.././images/programs/kindergarten-readiness/kindergarten-readiness5.jpg';
+import kindergartenReadiness6 from '.././images/programs/kindergarten-readiness/kindergarten-readiness6.jpg';
+import kindergartenReadiness7 from '.././images/programs/kindergarten-readiness/kindergarten-readiness7.jpg';
+import kindergartenReadiness8 from '.././images/programs/kindergarten-readiness/kindergarten-readiness8.jpg';
+import kindergartenReadiness9 from '.././images/programs/kindergarten-readiness/kindergarten-readiness9.jpg';
+import kindergartenReadiness10 from '.././images/programs/kindergarten-readiness/kindergarten-readiness10.jpg';
+import { sliderImg } from '.././components/styles/layout.module.scss'
+
 
 const KRPage = () => {
     
@@ -15,15 +29,67 @@ const KRPage = () => {
     const metaDesc = "We provide a nurturing, caring and safe environment for your child to learn and grow through age-appropriate experiences for children 4 years to 5 years.";
     const ageRange = "4 years to 5 years";
 
-
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
 
     return (
       <Layout pageTitle={pageTitle} metaDesc={metaDesc}>
+         <Helmet>
+          <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </Helmet>
         <StaticImage src=".././images/home/kindergartenReadiness.jpg" width={500} className={introImage}/>
         <h2 id="age"  style={{marginTop: "0"}}>{ageRange}</h2>
         <p>Here at Columbia Children's Center we provide a nurturing, caring and safe environment for your child to learn and grow through age-appropriate experiences. We are very proud of our Kindergarten Readiness Program. Our goal is to enhance your child's confidence and prepare them for a successful school experience. We believe that warmth is a vital element for a growing child. Our curriculum is designed so that your child will discover that learning is fun while making new friends and building strong relationships with their teachers who deeply care about their happiness and education.</p>
         <p>We have a variety of Centers in our classroom. Your child will be involved in hands-on and interactive activities to develop their emotional, social, and academic skills. They will be provided many opportunities to explore, communicate and create in a warm and loving atmosphere.</p>
-        <ProgramSlider programImages="kindergarten-readiness"/>
+        {/* <ProgramSlider programImages="kindergarten-readiness"/> */}
+
+        <Slider {...settings}>
+          <img className={sliderImg} src={kindergartenReadiness1} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness2} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness3} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness4} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness5} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness6} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness7} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness8} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness9} alt="Kindergarten Readiness" />
+          <img className={sliderImg} src={kindergartenReadiness10} alt="Kindergarten Readiness" />
+        </Slider>    
+
         <AdayIn pageTitle={pageTitle} >
           <div>
             <ul>
--- src/pages/preschool.js
+++ src/pages/preschool.js
@@ -8,7 +8,20 @@ import JSONDat from ".././data/adayin/preschool-yellow.json"
 import { StaticImage } from 'gatsby-plugin-image'
 import { introImage } from '.././components/styles/page.module.scss'
 import { employeesWrap } from '.././components/styles/employee.module.scss'
-
+import Slider from "react-slick";
+import { Helmet } from 'react-helmet';
+import preschool1 from '.././images/programs/preschool/preschool1.jpg';
+import preschool2 from '.././images/programs/preschool/preschool2.jpg';
+import preschool3 from '.././images/programs/preschool/preschool3.jpg';
+import preschool4 from '.././images/programs/preschool/preschool4.jpg';
+import preschool5 from '.././images/programs/preschool/preschool5.jpg';
+import preschool6 from '.././images/programs/preschool/preschool6.jpg';
+import preschool7 from '.././images/programs/preschool/preschool7.jpg';
+import preschool8 from '.././images/programs/preschool/preschool8.jpg';
+import preschool9 from '.././images/programs/preschool/preschool9.jpg';
+import preschool10 from '.././images/programs/preschool/preschool10.jpg';
+import preschool11 from '.././images/programs/preschool/preschool11.jpg';
+import { sliderImg } from '.././components/styles/layout.module.scss'
 
 const PPage = () => {
     
@@ -16,15 +29,54 @@ const PPage = () => {
   const metaDesc = "Our goal is to provide a fun, safe, positive environment; emphasizing on confidence building and self-esteem for children 3 years to 4 years.";
   const ageRange = "3 years to 4 years";
   
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
     return (
       <Layout pageTitle={pageTitle} metaDesc={metaDesc}>
+        <Helmet>
+          <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </Helmet>
         <StaticImage src=".././images/home/preschool.jpg" width={500} className={introImage}/>
         <h2 id="age"  style={{marginTop: "0"}}>{ageRange}</h2>
         <p>Our goal is to provide a fun, safe, positive environment; emphasizing on confidence building and self-esteem. The Preschool Program concentrates on listening and basic cognitive skills such as colors, shapes and alphabet and number recognition.</p>
         <p>Our daily schedule includes circle time, which promotes social development, sharing, cooperation and participation as a group. Math and Science activities reinforce practice counting, recognizing numbers, colors, sizes and shapes, as well as scissor practice. Story time and a language arts activity promote language development, alphabet and name recognition, individual expression and creativity. We have music and movement to develop skills of expression, rhythm, listening, coordination, as well as large motor skills.</p>
         <p>Our classroom has centers such as Science, Reading, Dramatic Play, Manipulative, and Art. These centers encourage your child's learning experience. Our maximum teacher to child ratio is 1:12.</p>
-        <ProgramSlider programImages="preschool"/>
+        {/* <ProgramSlider programImages="preschool"/> */}
         <AdayIn pageTitle="Preschool">
           <div>
             <h3>Yellow Group</h3>
@@ -37,6 +89,21 @@ const PPage = () => {
             </ul>
           </div> 
         </AdayIn>
+
+        <Slider {...settings}>
+          <img className={sliderImg} src={preschool1} alt="Preschool" />
+          <img className={sliderImg} src={preschool2} alt="Preschool" />
+          <img className={sliderImg} src={preschool3} alt="Preschool" />
+          <img className={sliderImg} src={preschool4} alt="Preschool" />
+          <img className={sliderImg} src={preschool5} alt="Preschool" />
+          <img className={sliderImg} src={preschool6} alt="Preschool" />
+          <img className={sliderImg} src={preschool7} alt="Preschool" />
+          <img className={sliderImg} src={preschool8} alt="Preschool" />
+          <img className={sliderImg} src={preschool9} alt="Preschool" />
+          <img className={sliderImg} src={preschool10} alt="Preschool" />
+          <img className={sliderImg} src={preschool11} alt="Preschool" />
+        </Slider>     
+
         <AdayIn pageTitle="Preschool" group="red">
           <div>
             <h3>Red Group</h3>
@@ -48,7 +115,7 @@ const PPage = () => {
               ))} 
             </ul>
           </div> 
-        </AdayIn>
+        </AdayIn>  
 {/* 
         <TuitionRates 
             pageTitle={pageTitle} 
