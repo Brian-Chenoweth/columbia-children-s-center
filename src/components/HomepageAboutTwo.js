2c0751e397f3839ad096954137fc60b658603852
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2023-03-11 16:15:09 -0800

    Homepage work

---
+++ src/components/HomepageAboutTwo.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { homepageAboutWrap, homepageAboutContent, one } from './styles/homepageAbout.module.scss'
+
+const homepageAboutTwo = ({ children }) => {
+  return (
+    <div className={homepageAboutWrap + ' ' + 'one'}>
+        <div className={homepageAboutContent}>
+          <p>We understand that choosing a child development program for your child can be a difficult decision. We hope that our 30 years of experience will help you to feel comfortable with your decision.</p>
+          <p>We believe that communication between parents and teachers is essential to the success of your child's education.</p>
+          <p>We believe that it takes a village to raise a child and we look forward to being a part of your family's village.</p>
+        </div>
+    </div>
+  )
+}
+
+export default homepageAboutTwo
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,16 @@ import Layout from '../components/layout'
 import HomepageSlider from '../components/homepageSlider'
 import HomepagePrograms from '../components/homepagePrograms'
 import HomepageAboutOne from '../components/HomepageAboutOne'
+import HomepageAboutTwo from '../components/HomepageAboutTwo'
 import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
 import './styles/index.module.scss'
-import { homepageSliderWrap, homepageSliderWrapTwo } from '.././components/styles/homepageSlider.module.scss'
+import { homepageSliderWrap, homepageSliderWrapTwo, homepageSliderWrapThree } from '.././components/styles/homepageSlider.module.scss'
 import { StaticImage } from 'gatsby-plugin-image'
 
-var kidsRaisingHands = require ('../images/home/kids-raising-hands.webp');
+var home1 = require ('../images/home/home1.jpg');
+var home2 = require ('../images/home/home2.jpg');
 var kidsSmiling = require ('../images/home/kids-smiling.webp');
-var cccFront = require ('../images/home/columbia-childrens-center-front.webp');
+var cccFront = require ('../images/home/outside.jpg');
 var logo = require ('../images/logo-small.png');
 
 const IndexPage = () => {
@@ -23,7 +25,7 @@ const IndexPage = () => {
 
         <ParallaxBanner
           layers={[
-            { image: kidsSmiling.default, speed: -60, style: {backgroundSize:'contain',} },
+            { image: home1.default, speed: -60, style: {backgroundSize:'contain',} },
             { image: logo.default, speed: -20, style: {backgroundRepeat:'no-repeat', backgroundSize:'200px, auto'} },
           ]}
           className="aspect-[2/1]"
@@ -34,18 +36,36 @@ const IndexPage = () => {
       
       <HomepageAboutOne />
 
+
       <div className={homepageSliderWrapTwo}>
 
       <ParallaxBanner
           layers={[
-            { image: kidsRaisingHands.default, speed: -60, style: {inset:'-225px, 0px'} },
+            { image: cccFront.default, speed: -60, style: {inset:'-225px, 0px'} },
           ]}
           className="aspect-[2/1]"
         />
 
       </div>
-      
+
+      <HomepageAboutTwo />
+
+
+      <div className={homepageSliderWrapThree}>
+
+      <ParallaxBanner
+          layers={[
+            { image: home2.default, speed: -60, style: {inset:'-225px, 0px'} },
+          ]}
+          className="aspect-[2/1]"
+        />
+
+      </div>
+
       <HomepagePrograms />
+
+      
+      
 
     </Layout>
 
