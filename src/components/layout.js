1c50c8f31cf837cc0673983bdda26ee0ab28b194
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2023-06-09 19:03:02 -0700

    More metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,6 +6,8 @@ import Navi from '../components/navi'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import favicon from ".././images/favicon.ico"
+import { Organization } from "schema-dts";
+import { helmetJsonLdProp } from "react-schemaorg";
 
 const Layout = ({ pageTitle, metaDesc, children }) => {
 
@@ -14,7 +16,30 @@ const Layout = ({ pageTitle, metaDesc, children }) => {
   return (
     <div className={pageTitle.toLowerCase()}>
       <main className={container}>
-        <Helmet htmlAttributes={{ lang: 'en' }}>
+        <Helmet htmlAttributes={{ lang: 'en' }}
+        script={[
+          helmetJsonLdProp<Organization>({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            name: "Columbia Children's Center",
+            url: "https://columbiachildrenscenter.com/",
+            logo: "https://columbiachildrenscenter.com/static/cd84102905614be0e50a022883c578c7/d3084/logo.webp",
+            description: "Columbia Children's Center offers childcare and preschool for children 18 months thru 5 years.",
+            address: {
+              "@type": "PostalAddress",
+              streetAddress: "840 E. Stowell Road",
+              addressLocality: "Santa Maria",
+              addressRegion: "CA",
+              postalCode: "93454",
+              addressCountry: "US"
+            },
+            telephone: "805-922-5437",
+            sameAs: [
+              "https://www.facebook.com/people/Columbia-Childrens-Center/100063793190740/",
+              "https://www.instagram.com/columbia.childrens.center/"
+            ],
+          }),
+        ]}>
           <link rel="icon" href={favicon} type="image/x-icon"/>
           <link rel="shortcut icon" href={favicon} type="image/x-icon"/>
           {(() => {
