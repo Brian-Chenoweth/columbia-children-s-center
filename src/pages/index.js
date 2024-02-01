5be16fd9c0aae2ef2f911ddf8d9fa7837af56b3c
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2024-01-31 19:48:49 -0800

    WeP usage on front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,76 +1,94 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import HomepageSlider from '../components/homepageSlider'
-import HomepagePrograms from '../components/homepagePrograms'
-import HomepageAboutOne from '../components/HomepageAboutOne'
-import HomepageAboutTwo from '../components/HomepageAboutTwo'
+import * as React from 'react';
+import Layout from '../components/layout';
+import HomepageSlider from '../components/homepageSlider';
+import HomepagePrograms from '../components/homepagePrograms';
+import HomepageAboutOne from '../components/HomepageAboutOne';
+import HomepageAboutTwo from '../components/HomepageAboutTwo';
 import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
-import './styles/index.module.scss'
+import './styles/index.module.scss';
 import { homepageSliderWrap, homepageSliderWrapTwo, homepageSliderWrapThree } from '.././components/styles/homepageSlider.module.scss'
-import { StaticImage } from 'gatsby-plugin-image'
+// Import your images with both WebP and fallback formats
+import home1WebP from '../images/home/home1.webp';
+import home1Fallback from '../images/home/home1.jpg';
+import home2WebP from '../images/home/home2.webp';
+import home2Fallback from '../images/home/home2.jpg';
+import cccFrontWebP from '../images/home/outside.webp';
+import cccFrontFallback from '../images/home/outside.jpg';
+import logo from '../images/logo-small.png';
 
-var home1 = require ('../images/home/home1.jpg');
-var home2 = require ('../images/home/home2.jpg');
-var kidsSmiling = require ('../images/home/kids-smiling.webp');
-var cccFront = require ('../images/home/outside.jpg');
-var logo = require ('../images/logo-small.png');
+function supportsWebP() {
+  if (typeof window !== 'undefined') {
+    const elem = document.createElement('canvas');
 
-const IndexPage = () => {
-  return (
+    if (elem.getContext && elem.getContext('2d')) {
+      return elem.toDataURL('image/webp').indexOf('data:image/webp') === 0;
+    }
+  }
 
-    <ParallaxProvider>
-    <Layout pageTitle="Columbia Children's Center" metaDesc="As soon as you enter Columbia Children&#039;s Center, you&#039;ll understand why families in Santa Maria have trusted us since 1992.">
-
-      <div className={homepageSliderWrap}>
-
-        <ParallaxBanner
-          layers={[
-            { image: home1.default, speed: -60, style: {backgroundSize:'contain',} },
-            { image: logo.default, speed: -20, style: {backgroundRepeat:'no-repeat', backgroundSize:'200px, auto'} },
-          ]}
-          className="aspect-[2/1]"
-        />
-
-      </div>
-
-      
-      <HomepageAboutOne />
-
-
-      <div className={homepageSliderWrapTwo}>
-
-      <ParallaxBanner
-          layers={[
-            { image: cccFront.default, speed: -60, style: {inset:'-225px, 0px'} },
-          ]}
-          className="aspect-[2/1]"
-        />
-
-      </div>
-
-      <HomepageAboutTwo />
-
-
-      <div className={homepageSliderWrapThree}>
-
-      <ParallaxBanner
-          layers={[
-            { image: home2.default, speed: -60, style: {inset:'-225px, 0px'} },
-          ]}
-          className="aspect-[2/1]"
-        />
-
-      </div>
-
-      <HomepagePrograms />
-
-      
-      
-
-    </Layout>
-
-    </ParallaxProvider>
-  )
+  return false;
 }
 
-export default IndexPage
+const IndexPage = () => {
+  const useWebP = supportsWebP();
+
+  return (
+    <ParallaxProvider>
+      <Layout
+        pageTitle="Columbia Children's Center"
+        metaDesc="As soon as you enter Columbia Children&#039;s Center, you&#039;ll understand why families in Santa Maria have trusted us since 1992."
+      >
+        <div className={homepageSliderWrap}>
+          <ParallaxBanner
+            layers={[
+              {
+                image: useWebP ? home1WebP : home1Fallback,
+                speed: -60,
+                style: { backgroundSize: 'contain' },
+              },
+              {
+                image: logo,
+                speed: -20,
+                style: { backgroundRepeat: 'no-repeat', backgroundSize: '200px, auto' },
+              },
+            ]}
+            className="aspect-[2/1]"
+          />
+        </div>
+
+        <HomepageAboutOne />
+
+        <div className={homepageSliderWrapTwo}>
+          <ParallaxBanner
+            layers={[
+              {
+                image: useWebP ? cccFrontWebP : cccFrontFallback,
+                speed: -60,
+                style: { inset: '-225px, 0px' },
+              },
+            ]}
+            className="aspect-[2/1]"
+          />
+        </div>
+
+        <HomepageAboutTwo />
+
+        <div className={homepageSliderWrapThree}>
+          <ParallaxBanner
+            layers={[
+              {
+                image: useWebP ? home2WebP : home2Fallback,
+                speed: -60,
+                style: { inset: '-225px, 0px' },
+              },
+            ]}
+            className="aspect-[2/1]"
+          />
+        </div>
+
+        <HomepagePrograms />
+      </Layout>
+    </ParallaxProvider>
+  );
+};
+
+export default IndexPage;
